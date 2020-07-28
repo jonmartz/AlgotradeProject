d205e1e816e1f64595699c1d6a625152e470c710
jonmartz <jonmartz91@gmail.com>
2020-07-28 14:08:33 +0300

    extracted features

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import csv
+import numpy as np
 
 # join all stocks according to their dates
-dataset = pd.read_csv('data/dataset_original.csv')
+dataset = pd.read_csv('data/original_dataset.csv')
 cols = dataset.columns
 stock_names = [cols[i].split('_')[0] for i in range(0, len(cols), 2)]
 start_date, end_date = dataset[cols[0]].iloc[[19, -1]]  # start from 20th date since there are no SPY before that
@@ -21,21 +21,39 @@ processed_dataset = processed_dataset.fillna(method='ffill').fillna(method='bfil
 days_per_block = 5
 block_start = 0
 stock_names = stock_names[:-1]
-x = []
-y = []
+rows = []
+last_SPY_by_block = []
+moving_avg_SPY_by_block = []
+weights = [1/(2 ** i) for i in range(days_per_block)]
 while block_start + days_per_block <= len(processed_dataset):
     block = processed_dataset[block_start:block_start + days_per_block]
     block_start_date, block_end_date = block['date'].iloc[[0, -1]]
     block_date_range = '%s:%s' % (block_start_date, block_end_date)
-    label = block['SPY'].mean()
+    # block_mean_spy = block['SPY'].mean()
+    row = [block_date_range]
     for i, stock_name in enumerate(stock_names):
-        one_hot = [0] * len(stock_names)
-        one_hot[i] = 1
-        stock_values = list(block[stock_name])
-        x.append([block_date_range] + one_hot + stock_values)
-        y.append(label)
+        # one_hot = [0] * len(stock_names)
+        # one_hot[i] = 1
+        # stock_values = list(block[stock_name])
+        # x.append([block_date_range] + one_hot + stock_values + [block_mean_spy])
+        # y.append(block_mean_spy)
+
+        stock_pct_change = block[stock_name].pct_change()
+        row.extend(list(stock_pct_change)[1:])
+    rows.append(row)
+    block_spy = list(block['SPY'])
+    last_SPY_by_block.append(block_spy[-1])
+    moving_avg_SPY_by_block.append(block_spy[0])
+    # moving_avg_SPY_by_block.append(np.average(block_spy, weights=weights))
     block_start += days_per_block
-cols = ['date range'] + stock_names + ['day %d' % (i + 1) for i in range(days_per_block)]
-final_dataset = pd.DataFrame(x[:-len(stock_names)], columns=cols)
-final_dataset['SPY'] = y[len(stock_names):]
-final_dataset.to_csv('data/processed_dataset.csv', index=False)
+
+# cols = ['date range'] + stock_names + ['day %d' % (i + 1) for i in range(days_per_block)] + ['current SPY']
+cols = ['date range']
+for stock_name in stock_names:
+    cols.extend(['%s day %d-%d' % (stock_name, i + 1, i + 2) for i in range(days_per_block-1)])
+processed_dataset = pd.DataFrame(rows[:-1], columns=cols)  # skip the last block
+# processed_dataset = pd.DataFrame(rows[:-len(stock_names)], columns=cols)  # skip the last block
+label_col_name = 'SPY day %d-%d avg' % (days_per_block + 1, 2 * days_per_block)
+label_col = np.array(moving_avg_SPY_by_block[1:])/np.array(last_SPY_by_block[:-1]) - 1  # SPY pct_change between blocks
+processed_dataset[label_col_name] = label_col
+processed_dataset.to_csv('data/processed_dataset.csv', index=False)
