27aa83dcc474c614dcb94bd8de0700561facc7be
jonmartz <jonmartz91@gmail.com>
2020-07-29 12:32:17 +0300

    implemented k-fold cross validation for all models

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
 
-# join all stocks according to their dates
+"""
+This script creates the dataset to train the models with.
+"""
+
+# JOIN ALL STOCKS ACCORDING TO THEIR DATES
 dataset = pd.read_csv('data/original_dataset.csv')
 cols = dataset.columns
 stock_names = [cols[i].split('_')[0] for i in range(0, len(cols), 2)]
 start_date, end_date = dataset[cols[0]].iloc[[19, -1]]  # start from 20th date since there are no SPY before that
-dates_range = pd.date_range(start_date, end_date, freq='B')
+dates_range = pd.date_range(start_date, end_date, freq='B')  # all days from start to end date
 date_index = [str(i).split(' ')[0] for i in dates_range]  # transform dates into strings
 processed_dataset = pd.DataFrame(index=date_index)  # start with blank dataset with all dates
 for stock_name in stock_names:  # join stock by stock
@@ -16,34 +20,42 @@ processed_dataset.columns = stock_names
 processed_dataset.index.rename('date', inplace=True)
 processed_dataset = processed_dataset.reset_index()  # set index as date column
 
-# aggregate dataset by number of days
+# AGGREGATE DATASET BY NUMBER OF DAYS
 processed_dataset = processed_dataset.fillna(method='ffill').fillna(method='bfill')  # fill forward then fill backward
 days_per_block = 5
 block_start = 0
 stock_names = stock_names[:-1]
 rows = []
-last_SPY_by_block = []
-moving_avg_SPY_by_block = []
-weights = [1/(2 ** i) for i in range(days_per_block)]
-while block_start + days_per_block <= len(processed_dataset):
+last_SPY_by_block = []  # will contain last SPY from each n day block
+moving_avg_SPY_by_block = []  # will contain moving average of SPY from each n day block
+weights = [1 / (2 ** i) for i in range(days_per_block)]  # weights for moving average
+while block_start + days_per_block <= len(processed_dataset):  # for each block
     block = processed_dataset[block_start:block_start + days_per_block]
     block_start_date, block_end_date = block['date'].iloc[[0, -1]]
-    block_date_range = '%s:%s' % (block_start_date, block_end_date)
-    row = [block_date_range]
+    block_date_range = '%s:%s' % (block_start_date, block_end_date)  # name of block
+    row = [block_date_range]  # to be inserted to the dataset
     for i, stock_name in enumerate(stock_names):
-        stock_pct_change = block[stock_name].pct_change()
+        stock_pct_change = block[stock_name].pct_change()  # we take only the pct_change of each stock
         row.extend(list(stock_pct_change)[1:])
     rows.append(row)
     block_spy = list(block['SPY'])
-    last_SPY_by_block.append(block_spy[-1])
-    moving_avg_SPY_by_block.append(np.average(block_spy, weights=weights))
+    last_SPY_by_block.append(block_spy[-1])  # append last SPY of block
+    moving_avg_SPY_by_block.append(np.average(block_spy, weights=weights))  # append moving average of SPY of block
     block_start += days_per_block
 
+# WRITE THE DATASET TO DISK
 cols = ['date range']
 for stock_name in stock_names:
-    cols.extend(['%s day %d-%d' % (stock_name, i + 1, i + 2) for i in range(days_per_block-1)])
+    cols.extend(['%s day %d-%d' % (stock_name, i + 1, i + 2) for i in range(days_per_block - 1)])
 processed_dataset = pd.DataFrame(rows[:-1], columns=cols)  # skip the last block
 label_col_name = 'SPY day %d-%d avg' % (days_per_block + 1, 2 * days_per_block)
-label_col = np.array(moving_avg_SPY_by_block[1:])/np.array(last_SPY_by_block[:-1]) - 1  # SPY pct_change between blocks
+label_col = np.array(moving_avg_SPY_by_block[1:]) / np.array(
+    last_SPY_by_block[:-1]) - 1  # SPY pct_change between last SPY of block i and moving average of SPY of block i+1
 processed_dataset[label_col_name] = label_col
 processed_dataset.to_csv('data/processed_dataset.csv', index=False)
+
+# SAVE ACTUAL VALUES OF SPY, TO LATER TRANSFORM THE PREDICTED PCT_CHANGE INTO ACTUAL SPY VALUES
+absolute_spy_values = pd.DataFrame({'date range': [row[0] for row in rows[:-1]],
+                                    'SPY last of current block': last_SPY_by_block[:-1],
+                                    'SPY moving avg of next block': moving_avg_SPY_by_block[1:]})
+absolute_spy_values.to_csv('data/absolute_spy_values.csv', index=False)
--- ModelSelection.py
+++ ModelSelection.py
@@ -1,63 +1,101 @@
+import csv
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
 from sklearn.tree import DecisionTreeRegressor
+import matplotlib.pyplot as plt
+
+
+def get_MSE_absolute(plot_predictions=False):
+    """
+    Take the predicted percentage changes of the test set and apply them to the values
+    of the SPY stock to get an MSE from the actual predicted SPY values.
+    :param plot_predictions: set to true to plot the predictions in the test set of the current fold
+    :return: MSE of the test predictions
+    """
+    spy_test = spy_values[test_index]
+    y_pred_absolute = spy_test[:, 0] * (1 + y_pred)
+    y_true_absolute = spy_test[:, 1]
+    if plot_predictions:
+        x = range(len(y_true_absolute))
+        plt.plot(x, y_true_absolute, marker='.', label='true SPY')
+        plt.plot(x, y_pred_absolute, marker='.', label='predicted SPY')
+        plt.xlabel('block index (5 days per block)')
+        plt.ylabel('SPY value')
+        plt.legend()
+        plt.title('Performance of %s, test set of one fold' % model_name)
+        plt.savefig('predicted vs true.png')
+        plt.show()
+    return mean_squared_error(y_true_absolute, y_pred_absolute)
+
+
+# LOAD DATASET AND SPLIT INTO X AND Y
 dataset = pd.read_csv('data/processed_dataset.csv')
 label_col_name = dataset.columns[-1]
-X = dataset.drop(columns=[dataset.columns[0], label_col_name]).to_numpy()
+X = dataset.drop(columns=['date range', label_col_name]).to_numpy()
 y = dataset[label_col_name].to_numpy()
-k_fold_cross_validation = KFold(n_splits=10)
-MSEs = []
-model_average_mse = []
-fold = 0
+# for getting actual values of the SPY in each block
+spy_values = pd.read_csv('data/absolute_spy_values.csv').drop(columns='date range').to_numpy()
 
-models = [linear_model.LinearRegression()]
+# SELECT ALL THE MODELS THAT WILL BE TESTED
+models = {}
+models['linear regression'] = linear_model.LinearRegression()
 # Adding different alpha values for Lasso and Ridge
-for alpha in range(1,10):
-    models.append(linear_model.Ridge(alpha=alpha/10.0))
-    models.append(linear_model.Lasso(alpha=alpha/10.0))
-
-# Adding SGD regressor with diferrent penalties
-models.append(linear_model.SGDRegressor(penalty='l2'))
-models.append(linear_model.SGDRegressor(penalty='l1'))
-models.append(linear_model.SGDRegressor(penalty='elasticnet'))
-
+for alpha in range(1, 10):
+    models['ridge alpha_%0.1f' % (alpha / 10.0)] = linear_model.Ridge(alpha=alpha / 10.0)
+    models['lasso alpha_%0.1f' % (alpha / 10.0)] = linear_model.Lasso(alpha=alpha / 10.0)
+# Adding SGD regressor with different penalties
+models['SGD l2'] = linear_model.SGDRegressor(penalty='l2')
+models['SGD l1'] = linear_model.SGDRegressor(penalty='l1')
+models['SGD elasticnet'] = linear_model.SGDRegressor(penalty='elasticnet')
 # Adding DecisionTreeRegressors with different maximum number of leaves
 for splitter in {'best', 'random'}:
-        models.append(DecisionTreeRegressor(max_leaf_nodes=None,splitter=splitter))
-        models.append(DecisionTreeRegressor(max_leaf_nodes=5,splitter=splitter))
-        models.append(DecisionTreeRegressor(max_leaf_nodes=10,splitter=splitter))
-        models.append(DecisionTreeRegressor(max_leaf_nodes=20,splitter=splitter))
-        models.append(DecisionTreeRegressor(max_leaf_nodes=30,splitter=splitter))
-        models.append(DecisionTreeRegressor(max_leaf_nodes=100,splitter=splitter))
+    models['tree splitter_%s max_leaf=None' % splitter] = DecisionTreeRegressor(max_leaf_nodes=None, splitter=splitter)
+    models['tree splitter_%s max_leaf=5' % splitter] = DecisionTreeRegressor(max_leaf_nodes=5, splitter=splitter)
+    models['tree splitter_%s max_leaf=10' % splitter] = DecisionTreeRegressor(max_leaf_nodes=10, splitter=splitter)
+    models['tree splitter_%s max_leaf=20' % splitter] = DecisionTreeRegressor(max_leaf_nodes=20, splitter=splitter)
+    models['tree splitter_%s max_leaf=30' % splitter] = DecisionTreeRegressor(max_leaf_nodes=30, splitter=splitter)
+    models['tree splitter_%s max_leaf=100' % splitter] = DecisionTreeRegressor(max_leaf_nodes=100, splitter=splitter)
 
-for model in models:
-    print("Model - %s" % model)
-    for train_index, test_index in k_fold_cross_validation.split(X):
+# PERFORM THE 10-FOLD CROSS VALIDATION FOR EACH MODEL
+k_fold_cross_validation = KFold(n_splits=10)
+MSEs_absolute = []  # MSEs relative to the actual SPY values
+MSEs_pct_change = []  # MSEs relative to the pct_change in SPY between blocks
+model_average_mse = []  # to save the MSE averaged of all the folds for each model
+with open('data/results.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['model name', 'MSE_absolute_avg', 'MSE_absolute_std', 'MSE_pct_change_avg', 'MSE_pct_change_std'])
+    for model_name, model in models.items():
+        print("model: %s" % model)
+        fold = 0
+        for train_index, test_index in k_fold_cross_validation.split(X):
+            fold += 1
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
 
-        fold += 1
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
+            # get predictions
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            MSE_absolute = get_MSE_absolute()
+            MSE_pct_change = mean_squared_error(y_test, y_pred)
+            MSEs_absolute.append(MSE_absolute)
+            MSEs_pct_change.append(MSE_pct_change)
+            print('\tfold=%d MSE=%.6f' % (fold, MSE_absolute))
 
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-        MSE = mean_squared_error(y_test, y_pred)
-        MSEs.append(MSE)
-        print('fold=%d MSE=%.6f' % (fold, MSE))
+        model_average_mse.append(np.mean(MSEs_absolute))
+        print('MSE_absolute_avg=%.6f MSE_absolute_std=%.6f | MSE_pct_change_avg=%.6f MSE_pct_change_std=%.6f\n'
+              % (np.mean(MSEs_absolute), np.std(MSEs_absolute), np.mean(MSEs_pct_change), np.std(MSEs_pct_change)))
 
-    model_average_mse.append(np.mean(MSEs))
-    print('MSE_avg=%.6f MSE_std=%.6f\n ' % (np.mean(MSEs), np.std(MSEs)))
+        writer.writerow([model_name, np.mean(MSEs_absolute), np.std(MSEs_absolute),
+                         np.mean(MSEs_pct_change), np.std(MSEs_pct_change)])
 
-minimal = None
-best = None
+# PRINT THE WINNING MODEL
+minimal_average_MSE = None
+best_model_name = None
 for mse_idx in range(len(model_average_mse)):
-    if minimal == None or minimal > model_average_mse[mse_idx]:
-        minimal = model_average_mse[mse_idx]
-        best = models[mse_idx]
-
-print("The Best model %s" % best)
-
-
-
+    if minimal_average_MSE is None or minimal_average_MSE > model_average_mse[mse_idx]:
+        minimal_average_MSE = model_average_mse[mse_idx]
+        best_model_name = models[list(models.keys())[mse_idx]]
+print("the best model is: %s" % best_model_name)
